d163acb10706f40a941e77ad3a46bb92f052aca1
HugeCoderGuy <alexscottlewis@gmail.com>
2022-05-22 18:52:03 -0700

    "she complied!"

--- Arduino_Projects/the_whole_enchalada/the_whole_enchalada.ino
+++ Arduino_Projects/the_whole_enchalada/the_whole_enchalada.ino
@@ -11,12 +11,13 @@
 
 
 /// ESP WIFI
+SoftwareSerial ESP8266(3, 4); // Rx,  Tx
+
 String myAPIkey = "6YCDUHSJGWKFG6L0";
 //String iq_air_key = "7cf2a06d-08f8-470d-9da2-7d63e31db776";
 //String aqi_url = "http://api.waqi.info/feed/Alameda/?token=6b47ef379c416b27e36c33d9e9d4095789221068";
 //String weatherbit_key = "47b0a5c7a0ba4c9bb10b4c6161315faa";
 
-SoftwareSerial ESP8266(3, 4); // Rx,  Tx
 
 // LCD Setup
 const int rs = 49, en = 48, d4 = 53, d5 = 52, d6 = 51, d7 = 50;
@@ -24,14 +25,16 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 #define LCD_Backlight 10 // 15. BL1 - Backlight +                                Emitter of 2N3904, Collector to VCC, Base to D10 via 10K resistor
 
 
-StaticJsonDocument<200> doc;
-
-#define DHTTYPE DHT11
-#define DHTPIN  A0
-//String PRAY = "GET /aq/observation/latLong/current/?format=text/csv&latitude=37.7878&longitude=-122.2744&distance=1&API_KEY=33628645-4EE3-4D27-A826-479EDB82E726\r\n\r\n";
-
+// setup DHT Sensor
+#define DHTTYPE     DHT11
+#define DHTPIN      A0
 DHT dht(DHTPIN, DHTTYPE,11);
-float humidity, temp_f, temp_c;
+
+//variable declaration
+byte humidity, temp_f, temp_c;
+byte house_plants = 0;
+byte jap_maple = 0;
+unsigned int light;
 long writingTimer = 17;
 long startTime = 0;
 long waitTime = 0;
@@ -43,11 +46,13 @@ unsigned char check_connection=0;
 unsigned char times_check=0;
 boolean error;
 
-/// Buttos ad lights
+/// Buttons and lights
 uint16_t hue =  0;
 bool pixel_state;
 #define LED    6
-int     button_switch =                       2; // external interrupt pin
+int     button_switch =                       18; // external interrupt pin
+int     tree_switch =                         19; // external interrupt pin
+int     house_switch =                        20; // external interrupt pin
 
 #define switched                            true // value if the button switch has been pressed
 #define triggered                           true // controls interrupt handler
@@ -57,16 +62,155 @@ int     button_switch =                       2; // external interrupt pin
 volatile  bool interrupt_process_status = {
   !triggered                                     // start with no switch press pending, ie false (!triggered)
 };
+volatile  bool tree_interrupt_process_status = {
+  !triggered                                     // start with no switch press pending, ie false (!triggered)
+};
+volatile  bool house_interrupt_process_status = {
+  !triggered                                     // start with no switch press pending, ie false (!triggered)
+};
 bool initialisation_complete =            false; // inhibit any interrupts until initialisation is complete
 
 
-// How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 4
-
 // Declare our NeoPixel strip object:
+#define LED_COUNT 4
 Adafruit_NeoPixel strip(LED_COUNT, LED);
 
-///////////// Fuctio Decleratio /////////////
+///////////// Program Initialization /////////////
+void setup()
+{
+  Serial.begin(9600);
+  ESP8266.begin(9600);
+  dht.begin();
+
+  bool pixel_state = false;
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
+  // setup LCD
+  lcd.begin(16, 2);
+
+  // button setup
+  pinMode(button_switch, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(button_switch),
+                  button_interrupt_handler,
+                  interrupt_trigger_type);
+
+  // plant buttons setup
+  attachInterrupt(digitalPinToInterrupt(tree_switch),
+                  button_interrupt_handler_tree,
+                  interrupt_trigger_type);
+  attachInterrupt(digitalPinToInterrupt(house_switch),
+                  button_interrupt_handler_house,
+                  interrupt_trigger_type);
+
+  // Wifi setup
+  startTime = millis();
+  ESP8266.println("AT+RST");
+  delay(2000);
+  Serial.println("Connecting to Wifi");
+  lcd.print(" Connecting to");
+  lcd.setCursor(0,1);
+  lcd.print("      Wifi...");
+   while(check_connection==0)
+  {
+    Serial.print(".");
+  ESP8266.print("AT+CWJAP=\"Portami-al-Mare\",\"184318aaea\"\r\n");
+  ESP8266.setTimeout(5000);
+  if(ESP8266.find("WIFI CONNECTED\r\n")==1)
+{
+    Serial.println("WIFI CONNECTED");
+    lcd.clear();
+    lcd.print("WIFI CONNECTED");
+    lcd.setCursor(0,1);
+    lcd.print("       :)");
+ break;
+ }
+ times_check++;
+ if(times_check>3)
+ {
+  times_check=0;
+   Serial.println("Trying to Reconnect..");
+  }
+  }
+  initialisation_complete = true; // open interrupt processing for business
+
+}
+
+void loop()
+{
+  waitTime = millis()-startTime;
+  if (waitTime > (writingTimer*1000))
+  {
+    readSensors();
+    lcd.clear();
+    lcd.print("    API CALL");
+    writeThingSpeak();
+    //writeAQIapi();
+
+    startTime = millis();
+  }
+
+  // test button switch and process if pressed
+  if (read_button() == switched) {
+    // button on/off cycle now complete, so flip LED between HIGH and LOW
+    pixel_state = !pixel_state;
+    lcd.clear();
+    lcd.print("Jellyfish On!");
+
+  } if (pixel_state == true) {
+    uint32_t rgbcolor = strip.ColorHSV(hue);
+    strip.fill(rgbcolor);
+    strip.show();
+    hue += 25;
+    // Prevet overflow of hue variable
+    if (hue >= 65534) {
+      hue = 0;
+    }
+  } else {
+    lcd.clear();
+    lcd.print("Jellyfish Off!");
+    strip.fill(0, 0, 0);
+    strip.show();
+  }
+
+  if (read_button_tree == switched) {
+    jap_maple = 100;
+     }
+
+  if (read_button_tree == switched) {
+    house_plants = 100;
+    }
+  if (jap_maple == 100 or house_plants == 100){
+    writeThingSpeakPlants();
+    jap_maple = 0;
+    house_plants = 0;
+    }
+
+  // Save some energy by turning off the LCD at night
+  if (light <= 100) {
+    lcd.clear();
+    lcd.print("Goodnight Sweet");
+    lcd.setCursor(0,1);
+    lcd.print("     Prince     ");
+    for (int i = 128; i = 0; i += 2) {
+      analogWrite(LCD_Backlight, i);
+    }
+    lcd.noDisplay();
+  }
+  else {
+    analogWrite(LCD_Backlight, 128);
+
+    lcd.display();
+    lcd.clear();
+    lcd.print(" I Have Awaken! ");
+    lcd.setCursor(0,1);
+    lcd.print("       :)       ");
+    delay(1000);
+  }
+
+}
+
+///////////// Function Declaration /////////////
 
 /// ESP8266 Wifi fuctios
 void readSensors(void)
@@ -74,6 +218,8 @@ void readSensors(void)
   temp_c = dht.readTemperature();
   humidity = dht.readHumidity();
   temp_f = (temp_c * 1.8) + 32;
+  light = analogRead(A0);
+
   Serial.println(temp_c);
   Serial.println(temp_f);
 }
@@ -89,6 +235,19 @@ void writeThingSpeak(void)
   getStr += String(temp_f);
   getStr +="&field4=";
   getStr += String(humidity);
+  getStr += "\r\n\r\n";
+  GetThingspeakcmd(getStr);
+}
+
+void writeThingSpeakPlants(void)
+{
+  startThingSpeakCmd();
+  String getStr = "GET /update?api_key=";
+  getStr += myAPIkey;
+  getStr +="&field5=";
+  getStr += String(house_plants);
+  getStr +="&field6=";
+  getStr += String(jap_maple);
   getStr += "\r\n\r\n";
   GetThingspeakcmd(getStr);
 }
@@ -208,14 +367,14 @@ String GetAQIapi(void)
     }
     Serial.print("MessageBody received: ");
     Serial.println(messageBody);
-    DeserializationError error = deserializeJson(doc, messageBody);
+    //DeserializationError error = deserializeJson(doc, messageBody);
 
     // Test if parsing succeeds.
-    if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
-    return;
-  }
+//    if (error) {
+//    Serial.print(F("deserializeJson() failed: "));
+//    Serial.println(error.f_str());
+//    return;
+//  }
     return messageBody;
   }
   
@@ -229,7 +388,7 @@ String GetAQIapi(void)
 
 
 
-// Butto iterrupt fuctios
+// Button interrupt functions
 //
 // ISR for handling interrupt triggers arising from associated button switch
 //
@@ -245,6 +404,38 @@ void button_interrupt_handler()
         // button pressed, so we can start the read on/off + debounce cycle wich will
         // be completed by the button_read() function.
         interrupt_process_status = triggered;  // keep this ISR 'quiet' until button read fully completed
+      }
+    }
+  }
+} // end of button_interrupt_handler
+void button_interrupt_handler_tree()
+{
+  if (initialisation_complete == true)
+  { //  all variables are initialised so we are okay to continue to process this interrupt
+    if (interrupt_process_status == !triggered) {
+      // new interrupt so okay start a new button read process -
+      // now need to wait for button release plus debounce period to elapse
+      // this will be done in the button_read function
+      if (digitalRead(tree_switch) == LOW) {
+        // button pressed, so we can start the read on/off + debounce cycle wich will
+        // be completed by the button_read() function.
+        tree_interrupt_process_status = triggered;  // keep this ISR 'quiet' until button read fully completed
+      }
+    }
+  }
+} // end of button_interrupt_handler
+void button_interrupt_handler_house()
+{
+  if (initialisation_complete == true)
+  { //  all variables are initialised so we are okay to continue to process this interrupt
+    if (interrupt_process_status == !triggered) {
+      // new interrupt so okay start a new button read process -
+      // now need to wait for button release plus debounce period to elapse
+      // this will be done in the button_read function
+      if (digitalRead(house_switch) == LOW) {
+        // button pressed, so we can start the read on/off + debounce cycle wich will
+        // be completed by the button_read() function.
+        house_interrupt_process_status = triggered;  // keep this ISR 'quiet' until button read fully completed
       }
     }
   }
@@ -278,115 +469,60 @@ bool read_button() {
   return !switched; // either no press request or debounce period not elapsed
 } // end of read_button function
 
-
-///////////// Program Iitializatio /////////////
-void setup()
-{
-  Serial.begin(9600);
-  ESP8266.begin(9600);
-  dht.begin();
-  
-  bool pixel_state = false;
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
-
-  // setup LCD
-  lcd.begin(16, 2);
-  
-  pinMode(button_switch, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(button_switch),
-                  button_interrupt_handler,
-                  interrupt_trigger_type);
-  startTime = millis();
-  ESP8266.println("AT+RST");
-  delay(2000);
-  Serial.println("Connecting to Wifi");
-  lcd.print(" Connecting to");
-  lcd.setCursor(0,1);
-  lcd.print("      Wifi...");
-   while(check_connection==0)
-  {
-    Serial.print(".");
-  ESP8266.print("AT+CWJAP=\"Portami-al-Mare\",\"184318aaea\"\r\n");
-  ESP8266.setTimeout(5000);
-  if(ESP8266.find("WIFI CONNECTED\r\n")==1)
-{
-    Serial.println("WIFI CONNECTED");
-    lcd.clear();
-    lcd.print("WIFI CONNECTED");
-    lcd.setCursor(0,1);
-    lcd.print("       :)");
- break;
- }
- times_check++;
- if(times_check>3)
- {
-  times_check=0;
-   Serial.println("Trying to Reconnect..");
-  }
-  }
-  initialisation_complete = true; // open interrupt processing for business
-  
-}
-
-void loop()
-{
-  waitTime = millis()-startTime;
-  if (waitTime > (writingTimer*1000))
-  {
-    readSensors();
-    lcd.clear();
-    lcd.print("    API CALL");
-    writeThingSpeak();
-    //writeAQIapi();
-
-    startTime = millis();
-  }
-
-  // test buton switch and process if pressed
-  if (read_button() == switched) {
-    // button on/off cycle now complete, so flip LED between HIGH and LOW
-    pixel_state = !pixel_state;
-    lcd.clear();
-    lcd.print("Jelly Light On!");
-
-  } if (pixel_state == true) {
-    uint32_t rgbcolor = strip.ColorHSV(hue);
-    strip.fill(rgbcolor);
-    strip.show();
-    hue += 25;
-    // Prevet overflow of hue variable
-    if (hue >= 65534) {
-      hue = 0;
+// Tree read butto
+bool read_button_tree() {
+  int button_reading;
+  // static variables because we need to retain old values between function calls
+  static bool     switching_pending = false;
+  static long int elapse_timer;
+  if (tree_interrupt_process_status == triggered) {
+    // interrupt has been raised on this button so now need to complete
+    // the button read process, ie wait until it has been released
+    // and debounce time elapsed
+    button_reading = digitalRead(tree_switch);
+    if (button_reading == LOW) {
+      // switch is pressed, so start/restart wait for button relealse, plus end of debounce process
+      switching_pending = true;
+      elapse_timer = millis(); // start elapse timing for debounce checking
     }
-  } else {
-    lcd.clear();
-    lcd.print("Jelly Light Off!");
-    strip.fill(0, 0, 0);
-    strip.show();
-  }
-
-  // Save some energy by turning off the LCD at night
-  int light = analogRead(A0);
-  if (light <= 100) {
-    lcd.clear();
-    lcd.print("Goodnight Sweet");
-    lcd.setCursor(0,1);
-    lcd.print("     Prince     ");
-    for (int i = 128; i = 0; i += 2) {
-      analogWrite(LCD_Backlight, i);
+    if (switching_pending && button_reading == HIGH) {
+      // switch was pressed, now released, so check if debounce time elapsed
+      if (millis() - elapse_timer >= debounce) {
+        // dounce time elapsed, so switch press cycle complete
+        switching_pending = false;             // reset for next button press interrupt cycle
+        tree_interrupt_process_status = !triggered; // reopen ISR for business now button on/off/debounce cycle complete
+        return switched;                       // advise that switch has been pressed
+      }
     }
-    lcd.noDisplay();
   }
-  else {
-    analogWrite(LCD_Backlight, 128);
+  return !switched; // either no press request or debounce period not elapsed
+} // end of read_button function
 
-    lcd.display();
-    lcd.clear();
-    lcd.print(" I Have Awaken! ");
-    lcd.setCursor(0,1);
-    lcd.print("       :)       ");
-    delay(1000);
+// houseplat read fuctio
+bool read_button_house() {
+  int button_reading;
+  // static variables because we need to retain old values between function calls
+  static bool     switching_pending = false;
+  static long int elapse_timer;
+  if (house_interrupt_process_status == triggered) {
+    // interrupt has been raised on this button so now need to complete
+    // the button read process, ie wait until it has been released
+    // and debounce time elapsed
+    button_reading = digitalRead(house_switch);
+    if (button_reading == LOW) {
+      // switch is pressed, so start/restart wait for button relealse, plus end of debounce process
+      switching_pending = true;
+      elapse_timer = millis(); // start elapse timing for debounce checking
+    }
+    if (switching_pending && button_reading == HIGH) {
+      // switch was pressed, now released, so check if debounce time elapsed
+      if (millis() - elapse_timer >= debounce) {
+        // dounce time elapsed, so switch press cycle complete
+        switching_pending = false;             // reset for next button press interrupt cycle
+        interrupt_process_status = !triggered; // reopen ISR for business now button on/off/debounce cycle complete
+        return switched;                       // advise that switch has been pressed
+      }
+    }
   }
-
-}
+  return !switched; // either no press request or debounce period not elapsed
+} // end of read_button function
